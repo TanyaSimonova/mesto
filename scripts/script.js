a7c8d3b24c3b529bb4f735c4979623f9edf16356
Tanya Simonova <tatyana.pnk@gmail.com>
2023-03-13 21:38:07 +0300

    hotfix: исправить код js для form-popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
 const editButtonElement = document.querySelector('.edit-button');
 const popupForm = document.querySelector('.popup');
 const popupContainerElement = document.querySelector('.popup-form__container');
-const formInputElement = popupContainerElement.querySelectorAll('.popup-form__input');
-
-const nameInput = popupContainerElement.querySelector('.popup-form__input_name');
-const jobInput = popupContainerElement.querySelector('.popup-form__input_speciality');
-
-const submitButtonElement = popupContainerElement.querySelector('.submit-button');
-const closeButtonElement = popupContainerElement.querySelector('.close-button');
+const nameInput = popupContainerElement.querySelector('.popup-form__input_type_name');
+const jobInput = popupContainerElement.querySelector('.popup-form__input_type_speciality');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__speciality');
+const closeButtonElement = popupForm.querySelector('.close-button');
 
 function openPopup() {
   popupForm.classList.add('popup_active');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 editButtonElement.addEventListener('click', openPopup);
@@ -21,20 +21,12 @@ function closePopup() {
 
 closeButtonElement.addEventListener('click', closePopup);
 
-
 function handleFormSubmit (evt) {
   evt.preventDefault();
-
-  nameInput.value;
-  jobInput.value;
-
-  let profileName = document.querySelector('.profile__name');
-  let profileJob = document.querySelector('.profile__speciality');
 
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 
   closePopup();
 }
-
-submitButtonElement.addEventListener('click', handleFormSubmit);
+popupContainerElement.addEventListener('submit', handleFormSubmit);
