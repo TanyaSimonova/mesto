84502b34b3fca6c76944fd47a811e4caff813b06
Tanya Simonova <tatyana.pnk@gmail.com>
2023-03-26 23:12:30 +0300

    feature: добавить popup для карточек

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,60 @@
 const editButtonElement = document.querySelector('.edit-button');
-const popupForm = document.querySelector('.popup');
+let popupForm = document.querySelectorAll('.popup');
+
+//const formProfile = document.querySelector('.popup-form__profile');
+//const formElements = document.querySelector('.popup-form__elements');
+
 const popupContainerElement = document.querySelector('.popup-form__container');
 const nameInput = popupContainerElement.querySelector('.popup-form__input_type_name');
 const jobInput = popupContainerElement.querySelector('.popup-form__input_type_speciality');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__speciality');
-const closeButtonElement = popupForm.querySelector('.close-button');
+let closeButtonElement = document.querySelectorAll('.close-button');
 
 const elementList = document.querySelector('.element-list');
 const itemTemplate = document.querySelector('.element-list__template').content;
 
-function openPopup() {
-  popupForm.classList.add('popup_active');
+const addButtonElement = document.querySelector('.add-button');
+const popupContainerPlace = document.querySelector('.popup-form__container_added');
+const placeInput = popupContainerPlace.querySelector('.popup-form__input_type_place');
+const linkInput = popupContainerPlace.querySelector('.popup-form__input_type_link');
+const placeName = document.querySelector('.element-list__title');
+const placeLink = document.querySelector('.element-list__image');
+
+function openPopup(index) {
+  popupForm[index].classList.add('popup_active');
+}
+
+function openPopupProfile() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
+  openPopup(0);
 }
 
-editButtonElement.addEventListener('click', openPopup);
+editButtonElement.addEventListener('click', ()=> openPopupProfile());
 
-function closePopup() {
-  popupForm.classList.remove('popup_active');
+addButtonElement.addEventListener('click', ()=> openPopup(1));
+
+
+function closePopup(index) {
+  popupForm[index].classList.remove('popup_active');
 }
 
-closeButtonElement.addEventListener('click', closePopup);
+closeButtonElement[0].addEventListener('click', ()=> closePopup(0));
+closeButtonElement[1].addEventListener('click', ()=> closePopup(1));
+
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 
-  closePopup();
+  closePopup(0);
 }
 popupContainerElement.addEventListener('submit', handleFormSubmit);
+
+
+
 
 const initialCards = [
   {
@@ -76,3 +98,19 @@ function renderElementsList(item) {
   copyElementList.querySelector('.element-list__image').alt = item.description;
   elementList.append(copyElementList);
 }
+
+function addCardPlace() {
+
+}
+
+/*function handleFormElementSubmit (evt) {
+  evt.preventDefault();
+  item.name = placeInput.value;
+  // = placeInput.value;
+  //placeLink.src = linkInput.value;
+  item.link = linkInput.value;
+
+  closePopup(1);
+}
+popupContainerPlace.addEventListener('submit', handleFormElementSubmit);
+*/
